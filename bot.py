eec8a001439b90cec4c42820fe8710d528d98f2f
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2025-01-03 03:05:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,6 +89,19 @@ class Bot(Client):
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
+     
+     def run(self):
+        """Run the bot."""
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.start())
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            self.LOGGER(__name__).info("Shutting down...")
+        finally:
+            loop.run_until_complete(self.stop())
+
 
     async def stop(self, *args):
         await super().stop()
